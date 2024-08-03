b2633904a2495ac94b472a9482d5eb0521917106
Pavitra Kondapally <pavitrakondapally@gmail.com>
2024-08-03 19:52:40 +0530

    added weather api

--- weather.js
+++ weather.js
@@ -1,9 +1,10 @@
 // weather.js
 const axios = require('axios');
-const API_KEY = 'your_openweathermap_api_key'; // Replace with your actual OpenWeatherMap API key
+require('dotenv').config();
+const API_KEY = process.env.WEATHER_API_KEY; // Replace with your actual OpenWeatherMap API key
 
 const getWeather = async (location) => {
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${'019dd82efc88791ea4bbceb763fcddca'}`;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
   try {
     const response = await axios.get(url);
     const { weather, main } = response.data;
